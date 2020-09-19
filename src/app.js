8b47c0a498f3f179a156b636591be5726fe542e0
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-20 03:22:05 +0530

    Complete jumbotron component

--- src/app.js
+++ src/app.js
@@ -7,9 +7,13 @@ function App() {
     <Jumbotron.Container>
       {jumboData.map((item) => (
         <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Title>{item.title}</Jumbotron.Title>
-          <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          <Jumbotron.Image src={item.image} alt={item.alt} />
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
         </Jumbotron>
       ))}
     </Jumbotron.Container>
