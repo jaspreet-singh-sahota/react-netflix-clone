ad9beb49e382a991529cd5c49dfd4d05f7e07312
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-20 03:39:29 +0530

    Rearrange the jumbotron component files structure

--- src/app.js
+++ src/app.js
@@ -1,23 +1,8 @@
-import React from 'react';
-import jumboData from './fixtures/jumbo'
-import Jumbotron from './components/jumbotron';
+import React from 'react'
+import JumbotronContainer from './containers/jumbotron'
 
 function App() {
-  return (
-    <Jumbotron.Container>
-      {jumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt} />
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+  return (<JumbotronContainer/>)
 }
 
-export default App;
+export default App
