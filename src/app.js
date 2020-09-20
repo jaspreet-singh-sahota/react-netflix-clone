d1dd58ce066c3b74ddac50bca003b2524513c68d
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-20 21:47:53 +0530

    Add footer container

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import JumbotronContainer from './containers/jumbotron'
+import FooterContainer from './containers/footer'
 
 function App() {
-  return (<JumbotronContainer/>)
+  return (<><JumbotronContainer/><FooterContainer/></>)
 }
 
 export default App
